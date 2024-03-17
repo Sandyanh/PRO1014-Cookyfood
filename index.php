02a8bd04749c70ed54fa6bb9ec883ea88a42b09f
Phuong Anh <phamphuonganh5112002@gmail.com>
2024-03-17 17:24:17 +0700

    style: Cập nhật giao diện trang chủ

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    include("dao/pdo.php");
+    include("site/header-site.php");
+    include("site/home-page.php");
+    include("site/footer-site.php");
+?>
---
+++ site/header-site.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cooky Market | Cooky</title>
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/page-container.css">
+</head>
+<body>
+    <div class="app">
+        <header class="header">
+            <div class="navigation-bar">
+                <div class="logo">
+                    <a href="./index.html">
+                        <img src="https://res.cloudinary.com/do9rcgv5s/image/upload/v1695381181/cooky%20market%20-%20PHP/cva2ntghjzrlryixcojp.svg" alt="Logo Cooky">
+                    </a>
+                </div>
+                <div class="search-input">
+                    <img class="icon-search" src="https://res.cloudinary.com/do9rcgv5s/image/upload/v1695381877/cooky%20market%20-%20PHP/lieirqymxmairjpyhrwj.svg" alt="Magnifying Glass">
+                    <input tabindex="0" type="text" placeholder="Tìm kiếm sản phẩm...">
+                </div>
+                <div class="user">
+                    <div class="download-app-button">
+                        Tải App Cooky
+                    </div>
+                    <div class="wishlist action n-btn" title="Danh sách yêu thích">
+                        <img class="icon" src="https://res.cloudinary.com/do9rcgv5s/image/upload/v1695386250/cooky%20market%20-%20PHP/v9hhpbadxib71owdbfkh.svg" alt="Wishlist">
+                    </div>
+                    <button class="cart-icon action n-btn" title="Giỏ hàng">
+                        <img class="icon" src="https://res.cloudinary.com/do9rcgv5s/image/upload/v1695386172/cooky%20market%20-%20PHP/fcmcexgvocebzmhuntfm.svg" alt="Cart">
+                    </button>
+                    <div class="phone action n-btn">
+                        <a href="tel:19002041">
+                            <img src="https://res.cloudinary.com/do9rcgv5s/image/upload/v1695386173/cooky%20market%20-%20PHP/u5u581opcqe1nlesw2bn.svg" alt="Hotline" class="icon">
+                        </a>
+                    </div>
+                    <div class="hotline action view-city">
+                        <span class="user-name">Hà Nội</span>
+                        <img src="https://res.cloudinary.com/do9rcgv5s/image/upload/v1695387068/cooky%20market%20-%20PHP/ww9hqjdjddhfcrgdiokz.svg" alt="toggle" class="icon toggle">
+                    </div>
+                    <div class="hotline action login">
+                        <img src="https://res.cloudinary.com/do9rcgv5s/image/upload/v1695381877/cooky%20market%20-%20PHP/wb5pyhdq2alh6cx8ml82.svg" alt="Login" class="icon">
+                        <a class="user-name" href="signin.php">
+                            <?php
+                            session_start();
+                            $username = isset($_SESSION['user']['username']) ? $_SESSION['user']['username'] : 'Đăng nhập';
+                            echo $username;
+                            ?>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>
+</body>
+</html>
